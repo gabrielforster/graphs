b8603e59f3f9f34f71eb33201670a1ce550d715f
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-11-26 01:28:00 -0300

    Feat/string on vertice label (#4)
    * it works with single char as a vertice
    
    * it finds a path (not the best)
    
    * rename vertices and edges methods
    
    * creating examples for "conexo" and "isomorfo"
    
    * can have vertices label as string
    
    * adjust usage of cmd lib
    
    * change spacing on matrix print
    
    * creating "caminho" example

---
+++ caminho.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "lib.h"
+#include "cmd.h"
+
+int main(int argc, char *argv[]) {
+  char temp_char1, temp_char2;
+  Grafo *grafo = (Grafo *) malloc(sizeof(Grafo));
+
+  if (argc < 3) {
+    printf("Usage: %s <arg1> <arg2> ... <argN>\n", argv[0]);
+    return 1;
+  }
+
+  int numElements;
+  char **vertices = pegar_vertices(argv[1], &numElements);
+
+  for (int i = 0; i < numElements; ++i) {
+    adicionar_vertice(grafo, vertices[i]);
+  }
+
+  free(vertices);
+
+  int numPares;
+  char ***arestas = pegar_arestas(argv[2], &numPares);
+
+  for (int i = 0; i < numPares; ++i) {
+    adicionar_aresta(grafo, arestas[i][0], arestas[i][1]);
+  }
+
+  for (int i = 0; i < numPares; ++i)
+    free(arestas[i]);
+
+  free(arestas);
+
+  imprimir_vertices(grafo);
+  imprimir_arestas(grafo);
+
+  imprimir_adjacencia_matriz(grafo);
+
+  Lista* lista = acha_caminho_bfs(grafo, argv[3], argv[4]);
+
+  printf("Caminho entre %s e %s usando bfs: ", argv[3], argv[4]);
+  if (lista == NULL) {
+    printf("Caminho não encontrado\n");
+    return 0;
+  } else {
+    while (lista != NULL) {
+      printf("%s ", lista->vertice.label);
+      lista = lista->proximo;
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
